5bde1822105b4513eb3657a3fd3e20f725798871
Christina Golovach <christina.golovach@gmail.com>
2018-11-09 16:25:06 +0300

    Change name of method

--- Logic/Search.cs
+++ Logic/Search.cs
@@ -1,28 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Logic
 {
     public static class Search
     {
-        public static int? BinSearch<T>(T[] array, T soughtValue)
+        public static int? BinarySearch<T>(T[] array, T soughtValue)
         {
+            CheckInputData(array, soughtValue);
+
+            CheckImplementationIComparable(soughtValue);
+
             var comparer = Comparer<T>.Default;
 
-            return BinSearch(array, soughtValue, comparer.Compare);
+            return BinarySearch(array, soughtValue, comparer.Compare);
         }
 
-
-        public static int? BinSearch<T>(T[] array, T soughtValue, IComparer<T> comparer)
+        public static int? BinarySearch<T>(T[] array, T soughtValue, IComparer<T> comparer)
         {
-            return BinSearch(array, soughtValue, comparer.Compare);
+            CheckInputData(array, soughtValue, comparer);
+
+            return BinarySearch(array, soughtValue, comparer.Compare);
         }
 
-        public static int? BinSearch<T>(T[] array, T soughtValue, Comparison<T> comparison)
+        public static int? BinarySearch<T>(T[] array, T soughtValue, Comparison<T> comparison)
         {
+            CheckInputData(array, soughtValue, comparison);
+
             int leftIndex = 0;
             int rightIndex = array.Length - 1;
             int midIndex = 0;
@@ -49,13 +53,45 @@ namespace Logic
             return null;
         }
 
+        private static void CheckInputData<T>(T[] array, T soughtValue, IComparer<T> comparer)
+        {
+            CheckInputData(array, soughtValue);
+
+            if (ReferenceEquals(comparer, null))
+            {
+                throw new ArgumentNullException($"The {nameof(comparer)} can not be null.");
+            }
+        }
+
+        private static void CheckInputData<T>(T[] array, T soughtValue, Comparison<T> comparison)
+        {
+            CheckInputData(array, soughtValue);
+
+            if (ReferenceEquals(comparison, null))
+            {
+                throw new ArgumentNullException($"The {nameof(array)} can not be null.");
+            }
+        }
+
+        private static void CheckInputData<T>(T[] array, T soughtValue)
+        {
+            if (ReferenceEquals(array, null))
+            {
+                throw new ArgumentNullException($"The {nameof(array)} can not be null.");
+            }
+
+            if (ReferenceEquals(soughtValue, null))
+            {
+                throw new ArgumentNullException($"The {nameof(soughtValue)} can not be null.");
+            }
+        }
+
         private static void CheckImplementationIComparable<T>(T soughtValue)
         {
             if (!typeof(IComparable<T>).IsAssignableFrom(typeof(T)))
             {
                 throw new ArgumentException($"The {typeof(T)} must implement the IComparable<{typeof(T)}> interface.");
             }
-
         }
     }
 }
